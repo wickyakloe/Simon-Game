1d25b6e46c5681b3e8f310ef660b5ef08c42943f
Wicky <wicky.akloe@gmail.com>
2019-05-07 16:52:40 +0000

    added random no generator and playlist generator

--- README.md
+++ README.md
@@ -11,7 +11,6 @@ For more information see this
 ## UX
 
 - As a user i can turn the game on and off
-- As a user i need to start the game
 - As a user i am presented with a random series of button presses.
 - As a user each time I input a series of button presses correctly, I see the 
 same series of button presses but with an additional step.
@@ -20,7 +19,6 @@ of button presses plays, and when I personally press a button.
 - As a user if I press the wrong button, I am notified that I have done so, and
 that series of button presses starts again to remind me of the pattern so I can try again.
 - As a user i can see how many steps are in the current series of button presses.
-- As a user if I want to restart, I can hit a button to do so, and the game will return to a single step.
 - As a user i can play in strict mode where if I get a button press wrong, it notifies
   me that I have done so, and the game restarts at a new random series of button presses.
 - As a  user i can win the game by getting a series of 20 steps correct. I am notified of my victory, then the game starts over.
--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    //Start Button 
+    //Start/on Button 
     $("#start").on("click", function() {
         $(this).css("background-color", "green");
         $("#red").addClass("light");
@@ -9,12 +9,12 @@ $(document).ready(function() {
         $("input").val("00");
     })
 
-    //Reset Button
+    //Reset/off Button
     $("#reset").on("mousedown", function() {
         $(this).css("background-color", "red");
         $("#start").removeAttr("style");
         $("input").val("");
-    })
+    });
 
     $("#reset").on("mouseup", function() {
         $(this).removeAttr("style");
@@ -22,5 +22,30 @@ $(document).ready(function() {
         $("#green").removeClass("light");
         $("#blue").removeClass("light");
         $("#yellow").removeClass("light");
-    })
+    });
+
 });
+
+
+var playlist = [];
+
+//Generate random number between 0 and 3
+function genRandomInt() {
+    var genNo = Math.floor(Math.random() * Math.floor(4));
+    return genNo;
+}
+
+//fill Playlist
+function fillPlaylist(){
+    if( playlist.length !== 0 ){
+        var lastInPlaylist = playlist[playlist.length -1].toString();
+        console.log(lastInPlaylist)
+        var newPlaylist = [];
+        newPlaylist.push(lastInPlaylist, genRandomInt().toString());
+        console.log(newPlaylist);
+        playlist.push(newPlaylist);
+
+    } else{
+        playlist.push([genRandomInt()]);
+    }
+}
